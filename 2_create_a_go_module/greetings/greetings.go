6a3e9eb9289b1ff3e2d80492afeef01c6c654f2f
ktodw98 <infuilamust17@gmail.com>
2026-01-31 16:04:48 -0800

    handle error

--- 2_create_a_go_module/greetings/greetings.go
+++ 2_create_a_go_module/greetings/greetings.go
@@ -1,9 +1,17 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If name is not given, return error
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	// Return a greeting that embeds the name in message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
