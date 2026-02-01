7b3ce9070c3c2045eddfc19a1cb6932b1c8b3f2a
ktodw98 <infuilamust17@gmail.com>
2026-01-31 16:37:22 -0800

    random format

--- 2_create_a_go_module/greetings/greetings.go
+++ 2_create_a_go_module/greetings/greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 func Hello(name string) (string, error) {
@@ -12,6 +13,16 @@ func Hello(name string) (string, error) {
 	}
 
 	// Return a greeting that embeds the name in message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. nice to meet you",
+		"How are you doing? %v",
+		"I'm goooood %v.",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
