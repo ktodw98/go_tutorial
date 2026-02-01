b3a074268ed5ad6ad789caa5ffee189b278f3502
ktodw98 <infuilamust17@gmail.com>
2026-01-31 16:52:58 -0800

    multiple slice

--- 2_create_a_go_module/hello/hello.go
+++ 2_create_a_go_module/hello/hello.go
@@ -11,11 +11,15 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Gwen")
+	names := []string{"Gwen", "Avigale", "Nas"}
+
+	messages, err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(message)
+	for _, message := range messages {
+		fmt.Println(message)
+	}
 }
