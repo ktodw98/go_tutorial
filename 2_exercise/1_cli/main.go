23aa7c74f0be8d9fc7753b27f695c9f52188f5b7
ktodw98 <infuilamust17@gmail.com>
2026-02-02 22:04:49 -0800

    error handling

--- 2_exercise/1_cli/main.go
+++ 2_exercise/1_cli/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -13,11 +14,17 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Println("Which file will you read?")
 
-	scanner.Scan()
-	link := scanner.Text()
+	if !scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+		log.Fatal("No input")
+	}
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+	link := strings.TrimSpace(scanner.Text())
+
+	if link == "" {
+		log.Fatal("No input")
 	}
 
 	// Read file
